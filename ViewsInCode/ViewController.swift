e1c2803658a887ea04cfcc40d6f05ffea8eb9799
Craig Grummitt <craiggrummitt@gmail.com>
2016-04-15 12:58:34 -0500

    Initial Commit

---
+++ ViewsInCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViewsInCode
+//
+//  Created by Craig Grummitt on 15/04/2016.
+//  Copyright © 2016 Craig Grummitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
