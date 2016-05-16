35805323cab201b955b5fbf8c44832d96dd83a99
Craig Grummitt <craiggrummitt@gmail.com>
2016-05-15 19:40:52 -0500

    Complete displaying views

--- ViewsInCode/ViewController.swift
+++ ViewsInCode/ViewController.swift
@@ -2,24 +2,39 @@
 //  ViewController.swift
 //  ViewsInCode
 //
-//  Created by Craig Grummitt on 15/04/2016.
+//  Created by Craig Grummitt on 13/04/2016.
 //  Copyright © 2016 Craig Grummitt. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    var redView:UIView!
+    var label:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+        view.backgroundColor = UIColor.yellowColor()
+        redView = UIView(frame: CGRect(x: 0, y: 0,
+            width: view.bounds.width, height: view.bounds.height / 2))
+        redView.backgroundColor = UIColor.redColor()
+        view.addSubview(redView)
+        
+        
+        label = UILabel(frame:
+            CGRect(x: 20, y: self.view.bounds.height / 2,
+                width: 20, height: 20))
+        label.backgroundColor = UIColor.orangeColor()
+        view.addSubview(label)
+        label.text = "Hello World"
+        label.font = label.font.fontWithSize(40)
+        label.sizeToFit()
 
+
+        print("View Did Load")
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
-
