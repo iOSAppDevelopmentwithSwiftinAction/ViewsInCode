4d7ac8f76057e74bc1bf7067eb8065e2745b8892
Craig Grummitt <craiggrummitt@gmail.com>
2017-03-08 16:05:51 -0500

    Updated for TD

--- ViewsInCode/ViewController.swift
+++ ViewsInCode/ViewController.swift
@@ -9,32 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var redView:UIView!
-    var label:UILabel!
+  var redView:UIView!
+  var label:UILabel!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = UIColor.yellow
+    redView = UIView(frame: CGRect(x: 0, y: 0,
+                                   width: view.bounds.width, height: view.bounds.height / 2))
+    redView.backgroundColor = UIColor.red
+    view.addSubview(redView)
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = UIColor.yellow
-        redView = UIView(frame: CGRect(x: 0, y: 0,
-            width: view.bounds.width, height: view.bounds.height / 2))
-        redView.backgroundColor = UIColor.red
-        view.addSubview(redView)
-        
-        
-        label = UILabel(frame:
-            CGRect(x: 20, y: self.view.bounds.height / 2,
-                width: 20, height: 20))
-        label.backgroundColor = UIColor.orange
-        view.addSubview(label)
-        label.text = "Hello World"
-        label.font = label.font.withSize(40)
-        label.sizeToFit()
-
-
-        print("View Did Load")
-    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    label = UILabel(frame:
+      CGRect(x: 20, y: self.view.bounds.height / 2,
+             width: 20, height: 20))
+    view.addSubview(label)
+    label.text = "Hello World"
+    label.font = label.font.withSize(40)
+    label.sizeToFit()
+  }
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
 }
